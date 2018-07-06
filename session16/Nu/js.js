037d762a90096b556f7e7f1817d0adaf229c8f68
duc huy <windyman1996@gmail.com>
2018-07-06 19:07:26 +0700

    session 16

---
+++ session16/Nu/js.js
@@ -0,0 +1,11 @@
+function bongda() {
+	document.getElementById('stnam').src="muasam.jpg";
+}
+
+function nhau() {
+	document.getElementById('stnam').src="dulich.jpg";
+}
+
+function cafe() {
+	document.getElementById('stnam').src="xemphim.jpg";
+}
